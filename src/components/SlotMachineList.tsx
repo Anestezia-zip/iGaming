5239640c990a75f4327309e9511779222f0e04da
Anestezia-zip <plekatybtc@gmail.com>
2025-01-12 13:22:51 +0200

    Created a state to store slot machines and their betting amounts

--- src/components/SlotMachineList.tsx
+++ src/components/SlotMachineList.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { SlotMachine, SlotMachineInterface } from '../classes/SlotMachine';
+import { mockSlotMachineData } from '../constants/index';
 
 const SlotMachineList = () => {
+    const [slotMachines, setSlotMachines] = useState<{
+        machine: SlotMachineInterface;
+        betAmounts: number[];
+    }[]>([]);
   return (
     <div>SlotMachineList</div>
   )
