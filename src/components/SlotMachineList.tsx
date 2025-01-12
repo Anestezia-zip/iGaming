70f06623046845d4eba29305c32dc9726865fe0b
Anestezia-zip <plekatybtc@gmail.com>
2025-01-12 13:29:25 +0200

    Added a state for tracking the selected slot machine, as well as a state for dynamic balance display

--- src/components/SlotMachineList.tsx
+++ src/components/SlotMachineList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SlotMachine, SlotMachineInterface } from '../classes/SlotMachine';
 import { mockSlotMachineData } from '../constants/index';
 
@@ -7,6 +7,12 @@ const SlotMachineList = () => {
         machine: SlotMachineInterface;
         betAmounts: number[];
     }[]>([]);
+    const [selectedSlotMachine, setSelectedSlotMachine] = useState<{
+        machine: SlotMachineInterface;
+        betAmounts: number[]
+    } | null>(null);
+    const [userBalance, setUserBalance] = useState(100); 
+    
   return (
     <div>SlotMachineList</div>
   )
