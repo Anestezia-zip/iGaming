5272c060b6a783fa1b9d0335e29f8e68978d138a
Anestezia-zip <plekatybtc@gmail.com>
2025-01-12 17:38:29 +0200

    Added the main interfaceof the second iframe. Handled the case where no slot machine is selected, displaying a fallback message. Displayed 'Try your luck' header. Showed details of the selected slot machine. Added buttons to select a bet, hover, and highlighted the currently selected button with specific styling.

--- src/components/SlotMachineGame.tsx
+++ src/components/SlotMachineGame.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import Button from './Button';
+import clsx from 'clsx';
 
 interface SlotMachineData {
     id: number;
@@ -8,27 +10,57 @@ interface SlotMachineData {
 
 const SlotMachineGame = () => {
     const [selectedMachine, setSelectedMachine] = useState<SlotMachineData | null>(null);
+    const [currentBet, setCurrentBet] = useState<number | null>(null);
+
 
     useEffect(() => {
-        // Слушаем сообщения из первого iframe
+        // Waiting for data on the selected machine
         const handleMessage = (event: MessageEvent) => {
             if (event.data.type === 'SELECT_MACHINE') {
                 setSelectedMachine(event.data.data);
-                // setCurrentBet(null); // Сбрасываем текущую ставку
-                // setResultMessage(null); // Очищаем прошлый результат
+                setCurrentBet(null); // Resetting the current rate
+                // setResultMessage(null); // Clearing the past result
             }
         };
 
         window.addEventListener('message', handleMessage);
         return () => window.removeEventListener('message', handleMessage);
     }, []);
+
+
     return (
-        <div>
+        <div className='container g7 min-h-[420px] p-2'>
+            <h2 className='caption text-xl py-6 pb-2'>Try your luck</h2>
             {selectedMachine ? (
-                <div>
-                    {selectedMachine.name}
-                </div>
-            ) : (<></>)}
+        <div className='container flex lg:gap-44 max-lg:gap-22 max-[695px]:gap-12 max-[410px]:gap-4 md:text-xl'>
+          <div>
+            <h3>Selected machine: {selectedMachine.name}</h3>
+            <p className='max-[570px]:hidden'>
+              Bet range: ${selectedMachine.betAmounts[0]} - {selectedMachine.betAmounts[selectedMachine.betAmounts.length - 1]}
+            </p>
+          </div>
+
+          <div className='md:text-xl'>
+              <p>Please select your bet:</p>
+            <div className='max-w-96 max-[695px]:max-w-60 grid grid-cols-4 gap-4 max-[695px]:grid-cols-2 max-[695px]:gap-2 my-4'>
+              {selectedMachine.betAmounts.map((bet, i) => (
+                <Button 
+                  key={i}
+                  onClick={() => setCurrentBet(bet)} 
+                  containerClassName={clsx('mb-8 max-[695px]:mb-4 rounded-14', currentBet === bet ? '!g2' : '')}
+                  size={clsx('py-3 px-4 max-[695px]:py-2 px-2')}
+                  textBtn=''
+                  hoverBg={currentBet !== bet}
+                >
+                  ${bet}
+                </Button>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h2 className='h5 mt-6 flex items-center justify-center'>Please select a slot machine from the list.</h2>
+      )}
         </div>
     )
 }
