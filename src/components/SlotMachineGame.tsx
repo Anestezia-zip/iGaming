a3f8a7e9ce00225df51d3542cd799bdcf5735350
Anestezia-zip <plekatybtc@gmail.com>
2025-01-13 21:07:25 +0200

    Removed unused declaration

--- src/components/SlotMachineGame.tsx
+++ src/components/SlotMachineGame.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Button from './Button';
 import clsx from 'clsx';
 import { SlotMachine } from '../classes/SlotMachine';
